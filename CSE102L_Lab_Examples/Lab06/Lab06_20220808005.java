0088a2c52c596eef36700ce602d4373917e204e5
Yahya Efe Kuruçay <109385388+efekurucay@users.noreply.github.com>
2024-03-28 09:13:33 +0300

    update

--- CSE102L_Lab_Examples/Lab06/Lab06_20220808005.java
+++ CSE102L_Lab_Examples/Lab06/Lab06_20220808005.java
@@ -51,9 +51,9 @@ interface Combustion{
 }
 
 interface Rentable{
-    Rentable rentOut(Gallery gallery):
+    //Rentable rentOut(Gallery gallery)
 
-    void returnVehice (Gallery gallery);
+    //void returnVehice (Gallery gallery);
 
 }
 
@@ -61,19 +61,75 @@ interface Diesel extends Combustion, Rentable{
 
 void cleanDieselFilter();
 //Method to clean the diesel filter
-}
-s
-abstract void Vehicle(){
 
 }
 
-interface Aircraft{
+abstract class Vehicle{
+
+
+    private String model; //The model of the vehicle
+    private int year; //The year of the vehicle
+    
+
+    Vehicle(String model, int year){
+        //Constructor to initialize the vehicle
+        this.model=model;
+        this.year=year;
+
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public abstract void startEngine();
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return "this.getClass().";
+    }
+
 
 
 }
 
 
-interface Ship{
+
+abstract class Aircraft extends Vehicle{
+
+    Aircraft(String model, int year){
+
+        super(model, year);
+
+        model=getModel();
+        year=getYear();
+
+
+    }
+
+    void fly(){
+        //Abstract method to define flying behavior
+
+    }
+
+}
+
+abstract class Ship extends Vehicle{
+
+    Ship(String model, int year) {
+        super(model, year);
+        //Constructor to initialize the Ship
+    }
+
+    void sail(){
+
+        //Abstract method to define sailing behavior
+    }
 
 
 
