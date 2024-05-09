27e51014db15b2c999eeab43040141a709b30f42
Yahya Efe Kuruçay <109385388+efekurucay@users.noreply.github.com>
2024-05-09 09:26:28 +0300

    update

--- CSE102L_Lab_Examples/Lab10/Lab10_20220808005.java
+++ CSE102L_Lab_Examples/Lab10/Lab10_20220808005.java
@@ -1,31 +1,83 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-
-/**
- * Lab10_20220808005
- */
-public class Lab10_20220808005 {
-
-    public static void main(String[] args) {
-        
-ArrayList<Integer> arrayList = new ArrayList<>();
-LinkedList<Integer> linkedList = new LinkedList<>();
-
-arrayList.add(0);
-arrayList.add(1);
-linkedList.add(0);
-linkedList.add(1);
-
-long start =System.currentTimeMillis();
-
-for (int i = 0; i < 1000000; i++) {
-  arrayList.add(i);
-
-}
-long end = System.currentTimeMillis();
-
-System.out.println(end-start);
+  import java.util.ArrayList;
+  import java.util.LinkedList;
 
 
-    }
-}
+
+
+  public class Lab10_20220808005 {
+
+      public static void main(String[] args) {
+          
+  ArrayList<Integer> arrayList = new ArrayList<>();
+  LinkedList<Integer> linkedList = new LinkedList<>();
+
+  arrayList.add(0);
+  arrayList.add(1);
+  linkedList.add(0);
+  linkedList.add(1);
+
+  long start =System.currentTimeMillis();
+
+  for (int i = 0; i < 1000000; i++) {
+    arrayList.add(i);
+
+  }
+  long end = System.currentTimeMillis();
+
+  System.out.println(end-start);
+
+
+      }
+  }
+
+
+  interface Collection<E>extends Iterable<E>{
+  boolean isEmpty();
+  int size();
+  }
+
+  interface Iterator<E>{
+    boolean hasNext();
+    E next();
+    void remove();
+  }
+
+  interface Iterable<E> {
+
+    Iterator<E> iterator();
+
+
+  }
+
+  interface Comparable<E>{
+
+    int compareTo(E other);
+  }
+
+  interface Comparator<E>{
+
+
+
+    int compare(E x, E y);
+  }
+
+interface ListIterator<E> extends Iterable <E>{  
+
+
+    void add(E elements);
+    boolean hasPrevious();
+    int nextIndex();
+    E previous();
+    int previousIndex();
+  }
+
+  interface List<E>extends Collection<E>{
+
+    boolean add(int index, E element);
+    boolean add(E e); 
+    E get(int index);
+    E set(int index, E element);
+    boolean remove (Object o);
+    
+  }
+
