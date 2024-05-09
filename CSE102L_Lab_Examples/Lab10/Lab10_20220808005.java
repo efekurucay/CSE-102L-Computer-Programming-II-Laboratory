5b0a6b43e98364b3d54748b10d29695be56669b4
Yahya Efe Kuruçay <109385388+efekurucay@users.noreply.github.com>
2024-05-09 09:42:06 +0300

    Update Lab10_20220808005.java

--- CSE102L_Lab_Examples/Lab10/Lab10_20220808005.java
+++ CSE102L_Lab_Examples/Lab10/Lab10_20220808005.java
@@ -1,39 +1,53 @@
-  import java.util.ArrayList;
-  import java.util.LinkedList;
 
-
+/**---------------------------------------------------
+ * Akdeniz University CSE102L Lab Examples
+ * author: Yahya Efe Kuruçay
+ * since: 09.05.2024
+ * Description: Lab10
+ * Website: https://efekurucay.com
+*---------------------------------------------------*/
+/***
+ *    ███████ ███████ ███████   
+ *    ██      ██      ██          
+ *    █████   █████   █████     
+ *    ██      ██      ██         
+ *    ███████ ██      ███████   
+ *                            
+ *                            
+ */
+import java.util.ArrayList;
+import java.util.LinkedList;
 
 
   public class Lab10_20220808005 {
 
       public static void main(String[] args) {
           
-  ArrayList<Integer> arrayList = new ArrayList<>();
-  LinkedList<Integer> linkedList = new LinkedList<>();
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        LinkedList<Integer> linkedList = new LinkedList<>();
 
-  arrayList.add(0);
-  arrayList.add(1);
-  linkedList.add(0);
-  linkedList.add(1);
+        arrayList.add(0);
+        arrayList.add(1);
+        linkedList.add(0);
+        linkedList.add(1);
 
-  long start =System.currentTimeMillis();
+        long start =System.currentTimeMillis();
 
-  for (int i = 0; i < 1000000; i++) {
-    arrayList.add(i);
+        for (int i = 0; i < 1000000; i++) {
+          arrayList.add(i);
 
-  }
-  long end = System.currentTimeMillis();
+        }
+        long end = System.currentTimeMillis();
 
-  System.out.println(end-start);
+        System.out.println(end-start);
 
 
       }
   }
 
-
   interface Collection<E>extends Iterable<E>{
-  boolean isEmpty();
-  int size();
+    boolean isEmpty();
+    int size();
   }
 
   interface Iterator<E>{
@@ -61,14 +75,14 @@
     int compare(E x, E y);
   }
 
-interface ListIterator<E> extends Iterable <E>{  
+  interface ListIterator<E> extends Iterable <E>{  
 
 
-    void add(E elements);
-    boolean hasPrevious();
-    int nextIndex();
-    E previous();
-    int previousIndex();
+      void add(E elements);
+      boolean hasPrevious();
+      int nextIndex();
+      E previous();
+      int previousIndex();
   }
 
   interface List<E>extends Collection<E>{
@@ -81,3 +95,74 @@ interface ListIterator<E> extends Iterable <E>{
     
   }
 
+  interface Queue<E>extends Collection<E>{
+        boolean offer (E o);
+        E peek();
+        E poll();
+
+    }
+
+  interface Stack<E>extends Collection<E>{
+
+    E peek();
+    E pop();
+    boolean push(E e);
+  }
+
+  interface Deque<E>extends Queue<E>{
+     boolean addFirst(E e);
+     boolean addLast(E e);
+
+     E getFirst();
+     E getLast();
+     E removeFirst();
+     E removeLast();
+
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /***
+ *              _____                    _____                    _____          
+ *             /\    \                  /\    \                  /\    \         
+ *            /::\    \                /::\    \                /::\    \        
+ *           /::::\    \              /::::\    \              /::::\    \       
+ *          /::::::\    \            /::::::\    \            /::::::\    \      
+ *         /:::/\:::\    \          /:::/\:::\    \          /:::/\:::\    \     
+ *        /:::/__\:::\    \        /:::/__\:::\    \        /:::/__\:::\    \    
+ *       /::::\   \:::\    \      /::::\   \:::\    \      /::::\   \:::\    \   
+ *      /::::::\   \:::\    \    /::::::\   \:::\    \    /::::::\   \:::\    \  
+ *     /:::/\:::\   \:::\    \  /:::/\:::\   \:::\    \  /:::/\:::\   \:::\    \ 
+ *    /:::/__\:::\   \:::\____\/:::/  \:::\   \:::\____\/:::/__\:::\   \:::\____\
+ *    \:::\   \:::\   \::/    /\::/    \:::\   \::/    /\:::\   \:::\   \::/    /
+ *     \:::\   \:::\   \/____/  \/____/ \:::\   \/____/  \:::\   \:::\   \/____/ 
+ *      \:::\   \:::\    \               \:::\    \       \:::\   \:::\    \     
+ *       \:::\   \:::\____\               \:::\____\       \:::\   \:::\____\    
+ *        \:::\   \::/    /                \::/    /        \:::\   \::/    /    
+ *         \:::\   \/____/                  \/____/          \:::\   \/____/     
+ *          \:::\    \                                        \:::\    \         
+ *           \:::\____\                                        \:::\____\        
+ *            \::/    /                                         \::/    /        
+ *             \/____/                                           \/____/         
+ *                                                                               
+ */
