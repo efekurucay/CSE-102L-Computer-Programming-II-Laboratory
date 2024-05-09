cbe6fb87cf9838b419c101b87945ee5ad44d9f45
Yahya Efe Kuruçay <109385388+efekurucay@users.noreply.github.com>
2024-05-09 09:54:02 +0300

    update

--- CSE102L_Lab_Examples/Lab10/Lab10_20220808005.java
+++ CSE102L_Lab_Examples/Lab10/Lab10_20220808005.java
@@ -15,7 +15,7 @@
  *                            
  *                            
  */
-import java.util.ArrayList;
+
 import java.util.LinkedList;
 
 
@@ -40,7 +40,15 @@ import java.util.LinkedList;
         long end = System.currentTimeMillis();
 
         System.out.println(end-start);
+        System.out.println("/////////////////");
 
+        Node<Integer>n1 = new Node (1);
+        Node<Integer>n2 = new Node (-4);
+        Node<Integer>n3 = new Node (6);
+        n2.next=n1;
+        n1.prev=n2;
+        n1.next=n3;
+        n3.prev=n1;
 
       }
   }
@@ -110,19 +118,86 @@ import java.util.LinkedList;
   }
 
   interface Deque<E>extends Queue<E>{
-     boolean addFirst(E e);
-     boolean addLast(E e);
+     
+    boolean addFirst(E e);
+    boolean addLast(E e);
 
-     E getFirst();
-     E getLast();
-     E removeFirst();
-     E removeLast();
+    E getFirst();
+    E getLast();
+    E removeFirst();
+    E removeLast();
 
 
   }
 
+  class Node<E>{
+    Node<E> prev;
+    Node<E> next;
+    E data;
 
+    Node(E data){
 
+      this.data=data;
+      next=null;
+    }
+  }
+
+  class ArrayList<E>implements List<E>{
+
+      private static final int DEFAULT_CAPATICY=10;
+
+      private Object[] elements;
+      private int size;
+
+      ArrayList(){
+
+        elements = new Object[DEFAULT_CAPATICY];
+        size=0;
+      }
+
+      @Override
+      public boolean isEmpty() {
+        throw new UnsupportedOperationException("Unimplemented method 'isEmpty'");
+      }
+
+      @Override
+      public int size() {
+        throw new UnsupportedOperationException("Unimplemented method 'size'");
+      }
+
+      @Override
+      public Iterator<E> iterator() {
+        throw new UnsupportedOperationException("Unimplemented method 'iterator'");
+      }
+
+      @Override
+      public boolean add(int index, E element) {
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+      }
+
+      @Override
+      public boolean add(E e) {
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+      }
+
+      @Override
+      public E get(int index) {
+        throw new UnsupportedOperationException("Unimplemented method 'get'");
+      }
+
+      @Override
+      public E set(int index, E element) {
+        throw new UnsupportedOperationException("Unimplemented method 'set'");
+      }
+
+      @Override
+      public boolean remove(Object o) {
+        throw new UnsupportedOperationException("Unimplemented method 'remove'");
+      }
+
+     
+
+  }
 
 
 
