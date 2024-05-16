b1230e1559e51a9782601d3e45fb457202b42f86
Yahya Efe Kuruçay <109385388+efekurucay@users.noreply.github.com>
2024-05-16 09:33:24 +0300

    update

--- CSE102L_Lab_Examples/Lab11/Lab11_20220808005.java
+++ CSE102L_Lab_Examples/Lab11/Lab11_20220808005.java
@@ -27,7 +27,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
 
- class Efe {
+public class Lab11_20220808005 {
     public static void main(String[] args) throws Exception {
         Set<String> fruits = new HashSet<>();
 
@@ -94,27 +94,7 @@ import java.util.*;
         
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-}
+ }
 
 
 
@@ -146,100 +126,18 @@ import java.util.*;
 
 
 
-class Lab11_20220808005 {
 
-    // Task 1: Word Frequency Counter
-    public static TreeMap<String, Integer> wordFrequencyCounter(String fileName) throws IOException {
-        TreeMap<String, Integer> wordFrequencyMap = new TreeMap<>();
 
-        BufferedReader reader = new BufferedReader(new FileReader(fileName));
-        String line;
-        while ((line = reader.readLine()) != null) {
-            line = line.toLowerCase().replaceAll("[^a-zA-Z ]", ""); // Remove punctuation and convert to lowercase
-            String[] words = line.split("\\s+"); // Split by whitespace
-            for (String word : words) {
-                wordFrequencyMap.put(word, wordFrequencyMap.getOrDefault(word, 0) + 1);
-            }
-        }
-        reader.close();
-        return wordFrequencyMap;
-    }
 
-    // Task 2: Reverse Mapping
-    public static <K, V> TreeMap<V, Set<K>> reverseMapping(Map<K, V> map) {
-        TreeMap<V, Set<K>> reversedMap = new TreeMap<>();
-        for (Map.Entry<K, V> entry : map.entrySet()) {
-            reversedMap.computeIfAbsent(entry.getValue(), k -> new TreeSet<>()).add(entry.getKey());
-        }
-        return reversedMap;
-    }
 
-    // Task 3: Find Most Frequent Set
-    public static <K, V> Map.Entry<K, Set<V>> findMostFrequentSet(TreeMap<K, Set<V>> treeMap, int n) {
-        for (Map.Entry<K, Set<V>> entry : treeMap.descendingMap().entrySet()) {
-            if (entry.getValue().size() == n) {
-                return entry;
-            }
-        }
-        return null; // Not found
-    }
 
-    // Task 4: Find All Sets of a Given Size
-    public static <K, V> Map<K, Set<V>> findAllSetsOfGivenSize(TreeMap<K, Set<V>> treeMap, int n) {
-        Map<K, Set<V>> result = new TreeMap<>();
-        for (Map.Entry<K, Set<V>> entry : treeMap.entrySet()) {
-            if (entry.getValue().size() == n) {
-                result.put(entry.getKey(), entry.getValue());
-            }
-        }
-        return result;
-    }
 
-    // Task 5: Find Kth Largest Set
-    public static <K, V> Map.Entry<K, Set<V>> findKthLargestSet(TreeMap<K, Set<V>> treeMap, int n, int k) {
-        int count = 0;
-        for (Map.Entry<K, Set<V>> entry : treeMap.descendingMap().entrySet()) {
-            if (entry.getValue().size() == n) {
-                count++;
-                if (count == k) {
-                    return entry;
-                }
-            }
-        }
-        return null; // Not found
-    }
 
-    public static void main(String[] args) throws IOException {
-        // Test the methods
-        String fileName = "shakespeare.txt"; // Provide the correct file path
-        TreeMap<String, Integer> wordFrequencyMap = wordFrequencyCounter(fileName);
-        System.out.println("Word Frequencies:");
-        for (Map.Entry<String, Integer> entry : wordFrequencyMap.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
-        }
 
-        // Test other methods with sample data
-        TreeMap<Integer, Set<String>> treeMap = new TreeMap<>();
-        treeMap.put(1, new HashSet<>(Arrays.asList("apple", "banana", "cherry")));
-        treeMap.put(2, new HashSet<>(Arrays.asList("apple", "banana")));
-        treeMap.put(3, new HashSet<>(Arrays.asList("apple", "orange")));
-        treeMap.put(4, new HashSet<>(Arrays.asList("banana", "cherry")));
-        treeMap.put(5, new HashSet<>(Arrays.asList("apple", "banana", "cherry", "date")));
 
-        System.out.println("\nReversed Mapping:");
-        System.out.println(reverseMapping(treeMap));
 
-        System.out.println("\nMost Frequent Set of Size 2:");
-        System.out.println(findMostFrequentSet(treeMap, 2));
 
-        System.out.println("\nAll Sets of Size 3:");
-        System.out.println(findAllSetsOfGivenSize(treeMap, 3));
-
-        System.out.println("\n2nd Largest Set of Size 3:");
-        System.out.println(findKthLargestSet(treeMap, 3, 2));
-    }
-}
-
+ 
 
 
 /*
